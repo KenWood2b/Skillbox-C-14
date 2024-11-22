53edac1f211e5dfcc8c0f5f733e5f9b0ef859f09
KenWood2b <kenwood2b@gmail.com>
2024-11-22 14:48:26 +0300

    First

---
+++ MultithreadingConsoleApp/MultithreadingConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultithreadingConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Программа начала выполнение.");
+            // Запуск нескольких потоков через Task
+            Task[] tasks = new Task[]
+            {
+                PrintMessageWithDelayAsync("Сообщение из первого потока", 1000),
+                PrintMessageWithDelayAsync("Сообщение из второго потока", 1500),
+                PrintMessageWithDelayAsync("Сообщение из третьего потока", 2000)
+            };
+
+            // Ждем завершения всех задач
+            await Task.WhenAll(tasks);
+
+            Console.WriteLine("Программа завершила выполнение.");
+        }
+
+        // Асинхронный метод, выполняющий вывод сообщения
+        static async Task PrintMessageWithDelayAsync(string message, int delay)
+        {
+            // Симуляция задержки
+            await Task.Delay(delay);
+
+            // Вывод сообщения и информации о потоке
+            Console.WriteLine($"{message} - ID потока: {Thread.CurrentThread.ManagedThreadId}");
+        }
+    }
+}
